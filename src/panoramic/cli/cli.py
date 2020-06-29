cff9e48c4ca03dab41ce8fa2c56869805da15bf3
Mark Nemec <mrknmc@me.com>
2020-06-29 16:30:47 +0200

    Add refresh endpoint support

--- src/panoramic/cli/cli.py
+++ src/panoramic/cli/cli.py
@@ -21,10 +21,16 @@ def cli(debug):
 
 @cli.command(help='Scan models from source')
 @click.argument('source-id', type=str, required=True)
-@click.option('--schema-filter', '-f', type=str, help='Filter down what schemas to scan')
-def scan(source_id: str, schema_filter: str):
+@click.option('--filter', '-f', type=str, help='Filter down what schemas to scan')
+def scan(source_id: str, table_filter: str):
     from panoramic.cli.scan import Scanner
+    from panoramic.cli.refresh import Refresher
     from panoramic.cli.write import write
 
-    tables = Scanner(source_id).run(schema_filter)
-    write(tables)
+    scanner = Scanner(source_id)
+    refresher = Refresher(source_id)
+    for table in scanner.scan_tables_names(table_filter):
+        table_name = f'{table["table_schema"]}.{table["table_name"]}'
+        refresher.refresh_table(table_name)
+        tables = scanner.scan_columns(table_filter=table_name)
+        write(tables)
