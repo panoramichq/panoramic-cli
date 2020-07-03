73127369ebc8299b00c7b261dfb2c16eed91da12
Mark Nemec <mrknmc@me.com>
2020-07-03 10:59:50 +0200

    Fix client tests depending on config file

--- tests/panoramic/cli/metadata/test_client.py
+++ tests/panoramic/cli/metadata/test_client.py
@@ -10,7 +10,9 @@ def test_create_get_columns_job():
         responses.POST, 'https://diesel/test-source/columns?table-filter=test-filter', json={'job_id': 'test-job-id'}
     )
 
-    client = MetadataClient(base_url='https://diesel/', token_url='https://token')
+    client = MetadataClient(
+        base_url='https://diesel/', token_url='https://token', client_id='client-id', client_secret='client-secret'
+    )
 
     assert client.create_get_columns_job('test-source', 'test-filter') == 'test-job-id'
 
@@ -22,7 +24,9 @@ def test_create_get_tables_job():
         responses.POST, 'https://diesel/test-source/tables?table-filter=test-filter', json={'job_id': 'test-job-id'}
     )
 
-    client = MetadataClient(base_url='https://diesel/', token_url='https://token')
+    client = MetadataClient(
+        base_url='https://diesel/', token_url='https://token', client_id='client-id', client_secret='client-secret'
+    )
 
     assert client.create_get_tables_job('test-source', 'test-filter') == 'test-job-id'
 
@@ -34,7 +38,9 @@ def test_create_refresh_job():
         responses.POST, 'https://diesel/test-source/refresh?table-name=test-table', json={'job_id': 'test-job-id'}
     )
 
-    client = MetadataClient(base_url='https://diesel/', token_url='https://token')
+    client = MetadataClient(
+        base_url='https://diesel/', token_url='https://token', client_id='client-id', client_secret='client-secret'
+    )
 
     assert client.create_refresh_job('test-source', 'test-table') == 'test-job-id'
 
@@ -44,7 +50,9 @@ def test_get_job_status():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
     responses.add(responses.GET, 'https://diesel/job/test-job-id', json={'job_status': 'RUNNING'})
 
-    client = MetadataClient(base_url='https://diesel/', token_url='https://token')
+    client = MetadataClient(
+        base_url='https://diesel/', token_url='https://token', client_id='client-id', client_secret='client-secret'
+    )
 
     assert client.get_job_status('test-job-id') == JobState.RUNNING
 
@@ -54,6 +62,8 @@ def test_get_job_results():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
     responses.add(responses.GET, 'https://diesel/job/test-job-id/results', json={'data': [{'a': 'b'}]})
 
-    client = MetadataClient(base_url='https://diesel/', token_url='https://token')
+    client = MetadataClient(
+        base_url='https://diesel/', token_url='https://token', client_id='client-id', client_secret='client-secret'
+    )
 
     assert client.get_job_results('test-job-id') == [{'a': 'b'}]
--- tests/panoramic/cli/test_command.py
+++
@@ -1,5 +0,0 @@
-from panoramic.cli.command import scan
-
-
-def test_scan():
-    scan('test-source', 'test-filter')
