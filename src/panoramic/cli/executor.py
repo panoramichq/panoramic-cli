e5b033d2c7f2f451ed7ba493903c3cee01fc7a01
Mark Nemec <mrknmc@me.com>
2020-07-08 17:56:44 +0200

    Add executor classes

--- src/panoramic/cli/executor.py
+++ src/panoramic/cli/executor.py
@@ -1,8 +1,41 @@
+from abc import ABC
 from typing import List, Tuple
 
-from panoramic.cli.state import Action, ActionList
+from panoramic.cli.state import Action, ActionList, Origin
+
+
+class Executor(ABC):
+
+    """Base executor class."""
+
+    def execute(self, action: Action):
+        raise NotImplementedError('execute not implemented')
+
+
+class LocalExecutor(Executor):
+
+    """Executes actions against local filesystem."""
+
+    def execute(self, action: Action):
+        pass
+
+
+class RemoteExecutor(Executor):
+
+    """Executes actions against remote."""
+
+    def execute(self, action: Action):
+        raise NotImplementedError('execute not implemented')
+
+
+def get_executor_for_direction(direction: Tuple[Origin, Origin]) -> Executor:
+    return RemoteExecutor() if direction[1] == Origin.REMOTE else LocalExecutor()
 
 
 def execute(actions: ActionList) -> Tuple[List[Action], List[Action]]:
     """Execute actions and return successful and failed actions."""
-    pass
+    executor = get_executor_for_direction(actions.direction)
+    for action in actions.actions:
+        executor.execute(action)
+
+    return [], []
