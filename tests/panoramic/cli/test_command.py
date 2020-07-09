c3c1f8cc01bc519f9ae111bb0fea23615666dc66
David Barton <k162+Z2l0aHVi@posteo.de>
2020-07-09 11:21:52 +0200

    Fix tests

--- tests/panoramic/cli/test_command.py
+++ tests/panoramic/cli/test_command.py
@@ -17,29 +17,29 @@ def mock_scanner():
         yield mock_scanner()
 
 
-@patch('panoramic.cli.command.write')
-def test_scan(mock_write, mock_scanner, mock_refresher):
+@patch('panoramic.cli.command.write_yaml')
+def test_scan(mock_write_yaml, mock_scanner, mock_refresher):
     mock_scanner.scan_tables.return_value = [{'table_schema': 'source.schema1', 'table_name': 'table1'}]
     mock_scanner.scan_columns.return_value = [
-        {'table_schema': 'schema1', 'table_name': 'table1', 'column_name': 'id'},
-        {'table_schema': 'schema1', 'table_name': 'table1', 'column_name': 'value'},
+        {'table_schema': 'source.schema1', 'table_name': 'table1', 'column_name': 'id', 'data_type': 'str'},
+        {'table_schema': 'source.schema1', 'table_name': 'table1', 'column_name': 'value', 'data_type': 'int'},
     ]
 
     scan('test-source', 'test-filter')
 
     assert mock_refresher.refresh_table.mock_calls == [call('schema1.table1')]
-    assert mock_write.mock_calls == [call(ANY)]
+    assert mock_write_yaml.mock_calls == [call(ANY, ANY)]
 
 
-@patch('panoramic.cli.command.write')
-def test_scan_single_table_error(mock_write, mock_scanner, mock_refresher):
+@patch('panoramic.cli.command.write_yaml')
+def test_scan_single_table_error(mock_write_yaml, mock_scanner, mock_refresher):
     mock_scanner.scan_tables.return_value = [{'table_schema': 'source.schema1', 'table_name': 'table1'}]
     mock_scanner.scan_columns.return_value = [
-        {'table_schema': 'schema1', 'table_name': 'table1', 'column_name': 'id'},
-        {'table_schema': 'schema1', 'table_name': 'table1', 'column_name': 'value'},
+        {'table_schema': 'source.schema1', 'table_name': 'table1', 'column_name': 'id', 'data_type': 'str'},
+        {'table_schema': 'source.schema1', 'table_name': 'table1', 'column_name': 'value', 'data_type': 'int'},
     ]
     mock_refresher.refresh_table.side_effect == [Exception('test'), None]
 
     scan('test-source', 'test-filter')
 
-    assert mock_write.mock_calls == [call(ANY)]
+    assert mock_write_yaml.mock_calls == [call(ANY, ANY)]
