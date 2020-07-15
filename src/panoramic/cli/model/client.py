43dfbada024371298db63c6d4aaecfb78cf9515c
Mark Nemec <mrknmc@me.com>
2020-07-15 13:49:33 +0200

    Change model client to use company_slug

--- src/panoramic/cli/model/client.py
+++ src/panoramic/cli/model/client.py
@@ -175,18 +175,14 @@ class ModelClient(OAuth2Client):
         """Delete model with a given name."""
         url = urljoin(self.base_url, name)
         logger.debug(f'Deleting model with name: {name}')
-        # TODO: change once company_slug works on API layer
-        # params = {'virtual_data_source': data_source, 'company_slug': company_slug}
-        params = {'virtual_data_source': 'my_facebook', 'company_slug': 'operam', 'company_id': '1'}
+        params = {'virtual_data_source': data_source, 'company_slug': company_slug}
         response = self.session.delete(url, params=params, timeout=5)
         response.raise_for_status()
 
     def upsert_model(self, data_source: str, company_slug: str, model: Model):
         """Add or update given model."""
         logger.debug(f'Upserting model with name: {model.name}')
-        # TODO: change once company_slug works on API layer
-        # params = {'virtual_data_source': data_source, 'company_slug': company_slug}
-        params = {'virtual_data_source': 'my_facebook', 'company_slug': 'operam', 'company_id': '1'}
+        params = {'virtual_data_source': data_source, 'company_slug': company_slug}
         response = self.session.put(self.base_url, json=model.to_dict(), params=params, timeout=5)
         response.raise_for_status()
 
@@ -194,15 +190,7 @@ class ModelClient(OAuth2Client):
         """Retrieve names of all models in a given source."""
         logger.debug(f'Listing names of models for source: {data_source}')
         url = urljoin(self.base_url, 'model-name')
-        # TODO: change once company_slug works on API layer
-        # params = {'virtual_data_source': data_source, 'company_slug': company_slug, 'offset': offset, 'limit': limit}
-        params = {
-            'virtual_data_source': 'my_facebook',
-            'company_id': '1',
-            'company_slug': 'operam',
-            'offset': offset,
-            'limit': limit,
-        }
+        params = {'virtual_data_source': data_source, 'company_slug': company_slug, 'offset': offset, 'limit': limit}
         response = self.session.get(url, params=params, timeout=5)
         response.raise_for_status()
         return response.json()['data']
@@ -210,14 +198,7 @@ class ModelClient(OAuth2Client):
     def get_models(self, data_source: str, company_slug: str, offset: int = 0, limit: int = 100) -> List[Model]:
         """Retrieve all models in a given source."""
         logger.debug(f'Listing models for source: {data_source}')
-        # params = {'virtual_data_source': data_source, 'company_slug': company_slug, 'offset': offset, 'limit': limit}
-        params = {
-            'virtual_data_source': 'my_facebook',
-            'company_id': '1',
-            'company_slug': 'operam',
-            'offset': offset,
-            'limit': limit,
-        }
+        params = {'virtual_data_source': data_source, 'company_slug': company_slug, 'offset': offset, 'limit': limit}
         response = self.session.get(self.base_url, params=params,)
         response.raise_for_status()
         return [Model.from_dict(d, virtual_data_source=data_source) for d in response.json()['data']]
--- tests/panoramic/cli/model/test_client.py
+++ tests/panoramic/cli/model/test_client.py
@@ -9,12 +9,10 @@ def mock_token_url(monkeypatch):
     monkeypatch.setenv('PANORAMIC_AUTH_TOKEN_URL', 'https://token')
 
 
-# TODO: Remove test skip once authz ready
-@pytest.mark.skip
 @responses.activate
 def test_delete_model():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
-    responses.add(responses.DELETE, 'https://diesel/model?virtual_data_source=test-source&company_id=test-company')
+    responses.add(responses.DELETE, 'https://diesel/model?virtual_data_source=test-source&company_slug=test-company')
 
     client = ModelClient(base_url='https://diesel/', client_id='client-id', client_secret='client-secret')
 
@@ -23,20 +21,19 @@ def test_delete_model():
     assert len(responses.calls) == 2
 
 
-# TODO: Remove test skip once authz ready
-@pytest.mark.skip
 @responses.activate
 def test_get_models():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
-    responses.add(responses.GET, 'https://diesel/', json={'data': [{'a': 'b'}]})
+    model = Model(
+        name='model', fully_qualified_object_name="db.schema.table", attributes=[], joins=[], visibility='available'
+    )
+    responses.add(responses.GET, 'https://diesel/', json={'data': [model.to_dict()]})
 
     client = ModelClient(base_url='https://diesel/', client_id='client-id', client_secret='client-secret')
 
-    assert client.get_models('test-source', 'test-company') == [{'a': 'b'}]
+    assert client.get_models('test-source', 'test-company') == [model]
 
 
-# TODO: Remove test skip once authz ready
-@pytest.mark.skip
 @responses.activate
 def test_get_model_names():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
@@ -47,12 +44,10 @@ def test_get_model_names():
     assert client.get_model_names('test-source', 'test-company') == ['a', 'b']
 
 
-# TODO: Remove test skip once authz ready
-@pytest.mark.skip
 @responses.activate
 def test_upsert_model():
     responses.add(responses.POST, 'https://token/', json={'access_token': '123123'})
-    responses.add(responses.PUT, 'https://diesel/?virtual_data_source=test-source&company_id=test-company', json={})
+    responses.add(responses.PUT, 'https://diesel/?virtual_data_source=test-source&company_slug=test-company', json={})
 
     client = ModelClient(base_url='https://diesel/', client_id='client-id', client_secret='client-secret')
 
