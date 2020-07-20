3dc298bbfc16157b9c088d81a3b1309b92fc38c6
Tomas Kislan <kislan.tomas@gmail.com>
2020-07-20 11:00:53 +0200

    Update src/panoramic/cli/cli.py
    Co-authored-by: Mark Nemec <mark.nemec@operam.com>

--- src/panoramic/cli/cli.py
+++ src/panoramic/cli/cli.py
@@ -30,7 +30,7 @@ def cli(debug):
 @cli.command(help='Scan models from source')
 @click.argument('source-id', type=str, required=True)
 @click.option('--filter', '-f', type=str, help='Filter down what schemas to scan')
-@click.option('--parallel', '-p', type=int, default=8, help='Parallelize table')
+@click.option('--parallel', '-p', type=int, default=8, help='Parallelize metadata scan')
 def scan(source_id: str, filter: Optional[str], parallel: Optional[int]):
     from panoramic.cli.command import scan
 
