45d5e7b4b04f90d031e3fb1f40f35a8088f3c5da
Mark Nemec <mrknmc@me.com>
2020-07-20 13:59:13 +0200

    Fix e2e scan test

--- e2e/panoramic/cli/test_scan_e2e.py
+++ e2e/panoramic/cli/test_scan_e2e.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
 
@@ -5,7 +7,8 @@ from panoramic.cli import cli
 
 
 @pytest.mark.vcr
-def test_scan_e2e():
+def test_scan_e2e(monkeypatch, tmpdir):
+    monkeypatch.chdir(Path('e2e') / 'scenarios' / 'pano-scan')
     runner = CliRunner()
 
     result = runner.invoke(cli, ['scan', 'SF', '--filter', 'METRICS3_STG.ADWORDS_VIEWS.ENTITY%'])
