eb9ea5610f61047ad5f49805bd3c0747997e36c9
Mark Nemec <mrknmc@me.com>
2020-07-23 19:19:03 +0200

    Fix test_scan_error_e2e test

--- e2e/panoramic/cli/test_scan_e2e.py
+++ e2e/panoramic/cli/test_scan_e2e.py
@@ -27,7 +27,5 @@ def test_scan_error_e2e(mock_scan, monkeypatch):
     result = runner.invoke(cli, ['scan', 'SF', '--filter', 'METRICS3_STG.ADWORDS_VIEWS.ENTITY%'])
 
     assert result.exit_code == 1
-    assert (
-        result.stdout
-        == """Error: Internal error occurred\nTraceback (most recent call last):\n  File "/Users/marknemec/code/pano-cli/src/panoramic/cli/cli.py", line 41, in scan\n    scan_command(source_id, filter, parallel)\n  File "/Users/marknemec/.pyenv/versions/3.8.2/lib/python3.8/unittest/mock.py", line 1081, in __call__\n    return self._mock_call(*args, **kwargs)\n  File "/Users/marknemec/.pyenv/versions/3.8.2/lib/python3.8/unittest/mock.py", line 1085, in _mock_call\n    return self._execute_mock_call(*args, **kwargs)\n  File "/Users/marknemec/.pyenv/versions/3.8.2/lib/python3.8/unittest/mock.py", line 1140, in _execute_mock_call\n    raise effect\nException: Test Exception\n\n"""
-    )
+    assert result.stdout.startswith('Error: Internal error occurred\nTraceback (most recent call last):\n')
+    assert result.stdout.endswith('raise effect\nException: Test Exception\n\n')
