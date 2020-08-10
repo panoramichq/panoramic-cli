d96b261ea7f690e85caae4af8cc1340c40101f5e
Mark Nemec <mrknmc@me.com>
2020-08-10 10:25:34 +0200

    Swap Diesel panoramicprod URL to panoramicapi

--- src/panoramic/cli/config/model.py
+++ src/panoramic/cli/config/model.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_URL = 'https://diesel.panoramicprod.com/api/v1/federated/model/'
+BASE_URL = 'https://diesel.panoramicapi.com/api/v1/federated/model/'
 
 
 def get_base_url() -> str:
