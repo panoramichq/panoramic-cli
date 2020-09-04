82d38e5458336591313a52f6746d680c7b4e14c4
Mike Christopher <mchristopher@users.noreply.github.com>
2020-09-04 10:45:49 -0700

    Update Travis build for S3 and add MIT to setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ setup(
     package_dir={"": "src"},
     python_requires=">=3.6",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
