6902b5188dc8b7782c7cf0e7d7fc96db56fa1251
Mike Christopher <mchristopher@users.noreply.github.com>
2020-11-09 09:29:23 -0800

    Fix homebrew build (#25)
    Fix the homebrew auto deploy script and ensure it runs correctly on release tags.

--- shipping/update_homebrew.sh
+++ shipping/update_homebrew.sh
@@ -4,7 +4,6 @@ generate_homebrew_formula() {
   rm -fr brewout
   mkdir -p brewout
   shipping/ppfg.sh -f -c shipping/ppfg -o brewout $(python shipping/generate-versions-file.py)
-  cat brewout/panoramic-cli.rb
 }
 
 setup_git() {
@@ -14,18 +13,19 @@ setup_git() {
 
 pull_homebrew_files() {
   rm -fr .homebrew_repo
-  git clone https://github.com/panoramichq/homebrew-brew.git .homebrew-git
-  ls -alh
+  git clone https://github.com/panoramichq/homebrew-brew.git .homebrew_repo
 }
 
 update_homebrew_formula() {
   cd .homebrew_repo || true
-  git checkout -b master
+  git checkout master || true
+  cd ..
   rm -fr .homebrew_repo/Formula/panoramic-cli.rb
   cp brewout/panoramic-cli.rb .homebrew_repo/Formula/panoramic-cli.rb
   cd .homebrew_repo
   git add Formula/panoramic-cli.rb
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  cd ..
 }
 
 upload_files() {
