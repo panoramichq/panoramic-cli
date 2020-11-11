c72a6d536f9714b84012afc6e77b5130101c04c0
Mark Nemec <mrknmc@me.com>
2020-11-11 14:55:27 +0100

    Test running CI in Github Actions

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     install_requires=[
         'panoramic-auth>=0.2.0',
