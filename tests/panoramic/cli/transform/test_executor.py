789e9eb3ba87bbb61d7bb96362f9bdbbcfa1d3e8
Roman Roštár <roman.rostar@gmail.com>
2020-12-14 16:25:26 +0100

    Remove autospec

--- tests/panoramic/cli/transform/test_executor.py
+++ tests/panoramic/cli/transform/test_executor.py
@@ -4,9 +4,9 @@ from panoramic.cli.transform.executor import TransformExecutor
 from panoramic.cli.transform.pano_transform import CompiledTransform, PanoTransform
 
 
-@patch('panoramic.cli.transform.executor.Connections.execute', autospec=True)
-@patch('panoramic.cli.transform.executor.Connections.get_by_name', autospec=True)
-@patch('panoramic.cli.transform.executor.get_dialect_credentials', autospec=True)
+@patch('panoramic.cli.transform.executor.Connections.execute')
+@patch('panoramic.cli.transform.executor.Connections.get_by_name')
+@patch('panoramic.cli.transform.executor.get_dialect_credentials')
 def test_executes_on_a_connection(mock_get_dialect_credentials, mock_connections_get_by_name, mock_connections_execute):
     transform = PanoTransform(name='test', fields=['a'], target='connection.schema.view_name')
     compiled_transform = CompiledTransform(transform=transform, company_slug='company_slug', compiled_query='q')
@@ -16,6 +16,7 @@ def test_executes_on_a_connection(mock_get_dialect_credentials, mock_connections
 
     TransformExecutor.execute(compiled_transform)
 
+    mock_connections_get_by_name.assert_called_with('connection')
     assert mock_connections_execute.mock_calls == [
         call(sql=compiled_transform.compiled_query, credentials=sentinel.credentials),
         call(sql=compiled_transform.correctness_query, credentials=sentinel.credentials),
