f66e0d7f6c38a18cc68af7265c2cba07cf7a6ff5
Roman Roštár <roman.rostar@gmail.com>
2020-12-15 16:52:07 +0100

    Remove redundant writer class

--- src/panoramic/cli/transform/commands.py
+++ src/panoramic/cli/transform/commands.py
@@ -10,7 +10,7 @@ from panoramic.cli.local.get import get_transforms
 from panoramic.cli.local.writer import FileWriter
 from panoramic.cli.paths import FileExtension, Paths
 from panoramic.cli.print import echo_error, echo_info
-from panoramic.cli.transform.compiler import CompiledQueryWriter, TransformCompiler
+from panoramic.cli.transform.compiler import TransformCompiler
 from panoramic.cli.transform.executor import TransformExecutor
 from panoramic.cli.transform.pano_transform import CompiledTransform, PanoTransform
 
@@ -61,6 +61,8 @@ def exec_command(
 
     transform_compiler = TransformCompiler(company_slug)
 
+    file_writer = FileWriter()
+
     echo_info('Compiling transforms...')
     with tqdm(transforms_with_path) as compiling_bar:
         for transform, transform_path in compiling_bar:
@@ -68,9 +70,7 @@ def exec_command(
                 compiled_transform = transform_compiler.compile(transform=transform)
                 compiled_transforms.append((compiled_transform, transform_path))
 
-                compiled_sql_path = CompiledQueryWriter.write(
-                    compiled_transform=compiled_transform, transform_path=transform_path
-                )
+                compiled_sql_path = file_writer.write_compiled_transform(compiled_transform)
 
                 compiling_bar.write(f'[{transform.name}] writing compiled query to {compiled_sql_path}')
             except Exception as e:
