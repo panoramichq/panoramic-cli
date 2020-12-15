868d8bca3329cdad84bc856ca2fa3a96776da114
Roman Roštár <roman.rostar@gmail.com>
2020-12-15 15:13:17 +0100

    Extract client in transform compiler to an instance prop

--- src/panoramic/cli/transform/compiler.py
+++ src/panoramic/cli/transform/compiler.py
@@ -6,16 +6,22 @@ from panoramic.cli.transform.pano_transform import CompiledTransform, PanoTransf
 
 
 class TransformCompiler:
-    @classmethod
-    def compile(cls, transform: PanoTransform, company_slug: str) -> CompiledTransform:
-        try:
-            transform_client = TransformClient()
+    company_slug: str
+    transform_client: TransformClient
 
-            compiled_query = transform_client.compile_transform(transform, company_slug, transform.connection_name)
+    def __init__(self, company_slug: str):
+        self.company_slug = company_slug
+        self.transform_client = TransformClient()
+
+    def compile(self, transform: PanoTransform) -> CompiledTransform:
+        try:
+            compiled_query = self.transform_client.compile_transform(
+                transform, self.company_slug, transform.connection_name
+            )
             create_view_statement = f"CREATE OR REPLACE VIEW {transform.view_path} AS ({compiled_query})"
 
             return CompiledTransform(
-                transform=transform, company_slug=company_slug, compiled_query=create_view_statement
+                transform=transform, company_slug=self.company_slug, compiled_query=create_view_statement
             )
         except RequestException as request_exception:
             raise TransformCompileException(transform.name).extract_request_id(request_exception)
