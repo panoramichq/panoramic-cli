ada678f01fb1b321326585acc89ce718cee1f073
Roman Roštár <roman.rostar@gmail.com>
2020-12-15 10:55:35 +0100

    Test execute fails without a connection

--- tests/panoramic/cli/transform/test_executor.py
+++ tests/panoramic/cli/transform/test_executor.py
@@ -2,7 +2,7 @@ from unittest.mock import call, patch, sentinel
 
 import pytest
 
-from panoramic.cli.errors import ConnectionFormatException
+from panoramic.cli.errors import ConnectionFormatException, ConnectionNotFound
 from panoramic.cli.transform.executor import TransformExecutor
 from panoramic.cli.transform.pano_transform import CompiledTransform, PanoTransform
 
@@ -29,7 +29,9 @@ def test_executes_on_a_connection(mock_get_dialect_credentials, mock_connections
 @patch('panoramic.cli.transform.executor.Connections.execute')
 @patch('panoramic.cli.transform.executor.Connections.get_by_name')
 @patch('panoramic.cli.transform.executor.get_dialect_credentials')
-def test_executes_on_a_connection(mock_get_dialect_credentials, mock_connections_get_by_name, mock_connections_execute):
+def test_execute_fails_with_malformed_credentials(
+    mock_get_dialect_credentials, mock_connections_get_by_name, mock_connections_execute
+):
     transform = PanoTransform(name='test', fields=['a'], target='connection.schema.view_name')
     compiled_transform = CompiledTransform(transform=transform, company_slug='company_slug', compiled_query='q')
 
@@ -40,4 +42,22 @@ def test_executes_on_a_connection(mock_get_dialect_credentials, mock_connections
         TransformExecutor.execute(compiled_transform)
 
     mock_connections_get_by_name.assert_called_with('connection')
+    assert mock_connections_execute.call_count == 0
+
+
+@patch('panoramic.cli.transform.executor.Connections.execute')
+@patch('panoramic.cli.transform.executor.Connections.get_by_name')
+@patch('panoramic.cli.transform.executor.get_dialect_credentials')
+def test_execute_fails_without_connection(
+    mock_get_dialect_credentials, mock_connections_get_by_name, mock_connections_execute
+):
+    transform = PanoTransform(name='test', fields=['a'], target='connection.schema.view_name')
+    compiled_transform = CompiledTransform(transform=transform, company_slug='company_slug', compiled_query='q')
+
+    mock_connections_get_by_name.side_effect = ValueError('connection')
+
+    with pytest.raises(ConnectionNotFound):
+        TransformExecutor.execute(compiled_transform)
+
+    assert mock_get_dialect_credentials.call_count == 0
     assert mock_connections_execute.call_count == 0
