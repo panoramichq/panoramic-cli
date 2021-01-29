036f45a05d39f5762088ce23dbe367b938192f79
Mike Christopher <mchristopher@users.noreply.github.com>
2021-01-29 13:58:57 -0800

    Update brew template, version 2.1.0

--- shipping/ppfg/template/install.rb
+++ shipping/ppfg/template/install.rb
@@ -1,5 +1,9 @@
 def install
   venv = virtualenv_create(libexec, "python3")
+  # Fix issues with Big Sur version detection
+  inreplace "#{libexec}/lib/python3.8/site-packages/pkg_resources/_vendor/packaging/version.py",
+    "match = self._regex.search(version)",
+    "match = self._regex.search(str(version))"
   venv.pip_install resources
   venv.pip_install buildpath
   (bin/"pano").write_env_script "#{libexec}/bin/pano", :RUNNING_UNDER_HOMEBREW => "1"
--- src/panoramic/cli/__version__.py
+++ src/panoramic/cli/__version__.py
@@ -1,2 +1,2 @@
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __minimum_supported_version__ = "2.0.0"
